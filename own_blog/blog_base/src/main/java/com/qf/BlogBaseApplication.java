effcf8bfb5367359323b47e95103e90563006ad3
ywh_name <1905577464@qq.com>
2019-12-12 12:03:45 +0800

    主框架完成

---
+++ own_blog/blog_base/src/main/java/com/qf/BlogBaseApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBaseApplication.class, args);
+    }
+
+}
