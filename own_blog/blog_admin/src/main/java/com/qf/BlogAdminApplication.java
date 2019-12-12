c0c09072fb4a497177f236780a813f3bc5335a4d
ywh_name <1905577464@qq.com>
2019-12-12 10:32:24 +0800

    主框架

---
+++ own_blog/blog_admin/src/main/java/com/qf/BlogAdminApplication.java
@@ -0,0 +1,15 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class BlogAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAdminApplication.class, args);
+    }
+
+}
